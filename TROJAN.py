a7169dbe6b72347a60ad9ba5475af29565c7e0e3
MandiYang <yangmandi2008@gmail.com>
2021-05-23 12:56:00 +0200

    UPDATE

--- TROJAN.py
+++ TROJAN.py
@@ -21,7 +21,7 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 root = tk.Tk()
 
-URL="https://raw.githubusercontent.com/MandiYang/YOUAREANIDIOT.PY.TROJAN/image/IDIOT.png"
+URL="https://raw.githubusercontent.com/MandiYang/YOUAREANIDIOT.PY.TROJAN/master/image/IDIOT.png"
 response = request.urlopen(URL, context=ctx)
 img_data = response.read()
 h=BytesIO(img_data)
